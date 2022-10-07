5bab8b99ca9097bdacc4baa702bc30e1a7908428
Leon Kurvers <kurvers.leon@gmail.com>
2022-10-07 16:53:22 +0200

    Copy Kevin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-const primaryNav = document.querySelector(".primary-navigation");
+// const primaryNav = document.querySelector(".primary-navigation");
+// const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// navToggle.addEventListener('click', () => {
+    
+//     const visibility = primaryNav.getAttribute("data-visible");
+//     if (visibility === "false") {
+//         primaryNav.setAttribute("data-visible", true);
+//     } else if (visibility == "true") {
+//         primaryNav.setAttribute("data-visible", false);
+//     }
+
+//     console.log(visibility);
+// });
+
+const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener("click", () => {
     
-    const visibility = primaryNav.getAttribute("data-visible");
-    if (visibility === "false") {
-        primaryNav.setAttribute("data-visible", true);
-    } else if (visibility == "true") {
-        primaryNav.setAttribute("data-visible", false);
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+    } else {
+        nav.setAttribute("data-visible", false);
     }
-
-    console.log(visibility);
-});
+})
